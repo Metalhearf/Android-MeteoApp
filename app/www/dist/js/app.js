cb2b1ffcd7b4dfea13de3943282a506900fdc181
Adrien <dasilvaadrien@hotmail.fr>
2015-12-07 00:10:47 +0100

    rework complet des 5 pages + prémices upload file
    ajout de modules ng, débuts uploadfile, remake css, 0.2 ready

--- app/www/dist/js/app.js
+++ app/www/dist/js/app.js
@@ -2,7 +2,7 @@ document.addEventListener('deviceready', function(){
 	//ici du code au lancement de l'application
 }, false)
 
-var app = angular.module('app', ['ngRoute']);
+var app = angular.module('app', ['ngRoute', 'ngFileUpload']);
 
 app.config(function($routeProvider){
 	$routeProvider
@@ -32,3 +32,27 @@ app.config(function($routeProvider){
 app.controller("NavController", function ($scope) {
 	$scope.menu = 'home';
 });
+
+app.controller('UploadController', ['$scope', 'Upload', '$timeout', function ($scope, Upload, $timeout) {
+    $scope.uploadFiles = function(file, errFiles) {
+        $scope.f = file;
+        $scope.errFile = errFiles && errFiles[0];
+        if (file) {
+            file.upload = Upload.upload({
+                url: 'https://angular-file-upload-cors-srv.appspot.com/upload',
+                data: {file: file}
+            });
+
+            file.upload.then(function (response) {
+                $timeout(function () {
+                    file.result = response.data;
+                });
+            }, function (response) {
+                if (response.status > 0)
+                    $scope.errorMsg = response.status + ': ' + response.data;
+            }, function (evt) {
+                file.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
+            });
+        }
+    }
+}]);

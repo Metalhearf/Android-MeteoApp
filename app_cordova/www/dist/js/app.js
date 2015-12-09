c8eee64121af0745ce95472cecb8b000a4b91bd5
Adrien <dasilvaadrien@hotmail.fr>
2015-12-09 16:45:16 +0100

    passage sous Ionic

---
+++ app_cordova/www/dist/js/app.js
@@ -0,0 +1,34 @@
+document.addEventListener('deviceready', function(){
+	//ici du code au lancement de l'application
+}, false)
+
+var app = angular.module('app', ['ngRoute', 'ngFileUpload']);
+
+app.config(function($routeProvider){
+	$routeProvider
+		.when('/home', {
+			templateUrl: 'views/home.html'
+		})
+
+		.when('/chart-moyenne', {
+			templateUrl: 'views/chart-moyenne.html'
+		})
+
+		.when('/chart-fulldata', {
+			templateUrl: 'views/chart-fulldata.html'
+		})
+
+		.when('/chart-windrose', {
+			templateUrl: 'views/chart-windrose.html'
+		})
+
+		.when('/about', {
+			templateUrl: 'views/about.html'
+		})
+
+		.otherwise({ redirectTo: '/home' })
+});
+
+app.controller("NavController", function ($scope) {
+	$scope.menu = 'home';
+});
---
+++ app_ionic/www/js/app.js
@@ -0,0 +1,35 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+var app = angular.module('app', ['ionic', 'ngRoute']);
+
+app.config(function($routeProvider){
+  $routeProvider
+    .when('/home', {
+      templateUrl: 'views/home.html'
+    })
+
+    .when('/chart-moyenne', {
+      templateUrl: 'views/chart-moyenne.html'
+    })
+
+    .when('/chart-fulldata', {
+      templateUrl: 'views/chart-fulldata.html'
+    })
+
+    .when('/chart-windrose', {
+      templateUrl: 'views/chart-windrose.html'
+    })
+
+    .when('/about', {
+      templateUrl: 'views/about.html'
+    })
+
+    .otherwise({ redirectTo: '/home' })
+});
+
+app.controller("NavController", function ($scope) {
+  $scope.menu = 'home';
+});
